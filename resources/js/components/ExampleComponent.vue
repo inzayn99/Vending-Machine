8ecb6890d8a5ac22905d0dca012af4cbc9c50d92
inzayn <hungrymindinside99@gmail.com>
2022-03-06 19:50:42 +0545

    Install laravel, Vue.js, npm-dev-watch

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+<!--                    <div class="card-header"><h1>Vending Machine</h1></b></div>-->
+                    <br>
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    mounted() {
+        console.log('Component mounted.')
+    }
+}
+</script>
